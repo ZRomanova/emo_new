c78f6acadcf4476852b65e04cc127acac25ac565
Zoya Romanova <Zoya@Mac-Book.local>
2020-09-21 21:11:12 +0300

    Message

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const http = require('./app')
 const port = process.env.PORT || 5000
 const host = process.env.HOST || 'localhost'
 
-http.listen(port, host, () => console.log(`Server has been started on ${port}`))
+http.listen(port, host, () => console.log(`Server has been started on ${host}:${port}`))
